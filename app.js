c06f5c9af14d8f6d20d4eac8f0d7cbd8360e2006
franco fresno <franco.fresno@gmail.com>
2022-10-31 21:13:28 -0300

    permito todo en cors

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ const usuarios = require("./src/routes/usuarios");
 const app = express();
 const PORT = 5000;
 
-const whitelist = ["http://localhost:3000", "https://cyberely-front.onrender.com"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
-app.use(cors(corsOptions));
+// const whitelist = ["http://localhost:3000", "https://cyberely-front.onrender.com"];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.append("Access-Control-Allow-Origin", "*");
   res.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.append("Access-Control-Allow-Headers", "X-Requested-With,content-type");
   res.append("Access-Control-Allow-Credentials", true);
