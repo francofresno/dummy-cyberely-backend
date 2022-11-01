52ddb2120dcaafae3a0f3b84523e26be552daf15
franco fresno <franco.fresno@gmail.com>
2022-11-01 00:04:36 -0300

    cors vuln

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -10,30 +10,45 @@ const {
 const router = express.Router();
 
 router.post("/crearUsuario", async function (req, res) {
+  const origin = req.get("origin");
+  res.set("Access-Control-Allow-Origin", origin);
+
   const { nombre, contraseña, email } = req.body;
   const response = await crearUsuario(nombre, contraseña, email);
   res.status(200).send(crearResponse(response));
 });
 
 router.get("/hacerComentario", async function (req, res) {
+  const origin = req.get("origin");
+  res.set("Access-Control-Allow-Origin", origin);
+
   const { usuarioId, comentario, social, nombre } = req.query;
   const response = await crearComentario(usuarioId, comentario, social, nombre);
   res.status(200).send(crearResponse(response));
 });
 
 router.get("/comentarios", async function (req, res) {
+  const origin = req.get("origin");
+  res.set("Access-Control-Allow-Origin", origin);
+
   const { usuarioId } = req.query;
   const response = await obtenerComentariosDe(usuarioId);
   res.status(200).send(crearResponse(response));
 });
 
 router.get("/", async function (req, res) {
+  const origin = req.get("origin");
+  res.set("Access-Control-Allow-Origin", origin);
+
   const { nombre } = req.query;
   const response = await obtenerUsuarioPorNombre(nombre);
   res.status(200).send(crearResponse(response));
 });
 
 router.get("/changePassword", async function (req, res) {
+  const origin = req.get("origin");
+  res.set("Access-Control-Allow-Origin", origin);
+
   const { newPassword, usuarioId } = req.query;
   await cambiarContraseña(newPassword, usuarioId);
   res.status(200).send(crearResponse("Password successfully changed"));
