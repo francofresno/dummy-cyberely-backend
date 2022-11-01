fb008b0c852247a9f1df15518da42e7f4d156e4c
franco fresno <franco.fresno@gmail.com>
2022-10-31 22:27:48 -0300

    request headers

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.append("Access-Control-Allow-Origin", "https://cyberely-front.onrender.com");
   res.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-  // res.append("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-  res.append("Access-Control-Allow-Headers", "*");
+  res.append("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+  // res.append("Access-Control-Allow-Headers", "*");
   res.append("Access-Control-Allow-Credentials", true);
   res.append("Content-Type", "application/json");
   next();
